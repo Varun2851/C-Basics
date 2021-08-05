683e9c928f931fc0bf9bbef91a0d0ca8b4fc7732
Varun Yadav <varunyadav891@gmail.com>
2021-08-05 23:36:36 +0530

    Tute

---
+++ Age.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int age;
+
+    cout<<"Varun :"<<endl;
+    cin>>age;
+
+    if(age>=200){
+        cout<<"You are eligible for voting :"<<endl;
+
+    }
+    else{
+        cout<<"You are not eligible :"<<endl;
+    }
+
+    cout<<endl;
+    return 0;
+
+}
+
+
+
+
+
+
